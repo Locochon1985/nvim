f586c36398f01a9861a8a17dc862ce8e75591618
Locochon1984 <maquiavelo585@gmail.com>
2024-08-27 10:11:11 -0300

    Version 2

--- lua/giuliano/plugins/navigation/neo-tree.lua
+++ lua/giuliano/plugins/navigation/neo-tree.lua
@@ -12,10 +12,10 @@ return {
         vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
         vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
         require("neo-tree").setup({
-            -- source_selector = {
-            --     winbar = true,
-            --     statusline = true,
-            -- },
+            source_selector = {
+                winbar = true,
+                statusline = true,
+            },
             close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
             popup_border_style = "rounded",
             enable_git_status = true,
