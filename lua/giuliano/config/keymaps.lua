e7aa05065c7b9fb6ae084253a11916e1470f1f1a
Locochon1985 <maquiavelo585@gmail.com>
2024-11-04 16:06:06 -0300

    lazyvim

--- lua/giuliano/config/keymaps.lua
+++ lua/giuliano/config/keymaps.lua
@@ -2,35 +2,15 @@ vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 -- themery
 keymap.set("n", "<leader>th", "<cmd>Themery<CR>", {})
--- cmp
-keymap.set({ "i", "s" }, "<c-k>", function()
-    if ls.expand_or_jumpable() then
-        ls.expand_or_jump()
-    end
-end, { silent = true })
-keymap.set({ "i", "s" }, "<c-j>", function()
-    if ls.jumpable(-1) then
-        ls.jump(-1)
-    end
-end, { silent = true })
 -- toggle term
 keymap.set("n", "<s-m>", "<cmd>ToggleTerm direction=float<CR>", { desc = "Toggle terminal" })
 --Basic
-keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 keymap.set("n", "<C-s>", "<cmd>w<CR>")
 keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
---telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {})
-keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", {})
-keymap.set("n", "<leader>fc", "<cmd>Telescope colorscheme<cr>", {})
-keymap.set("n", "<leader>fh", "<cmd>Telescope oldfiles<cr>", {})
---mini-files
-keymap.set("n", "<leader><leader>", "<cmd>lua=MiniFiles.open()<CR>", {})
---nvim-tree
+
 keymap.set("n", "<C-n>", "<cmd>Neotree toggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-keymap.set("n", "<leader>ee", "<cmd>Neotree focus<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
 -- window management
 keymap.set("n", "<leader>oo", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>xx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<Tab>", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
